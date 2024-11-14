5f624edac0a0a0bdfd47b3cc6adc0a81d51d6eae
Brian Pepple <bdpepple@gmail.com>
2024-11-14 09:51:32 -0500

    Improve data types (#58)
    * Change `PageCount` element type to nonNegativeInteger
    * Change Series `Volume` sub-element type to nonNegativeInteger

--- tests/test.py
+++ tests/test.py
@@ -8,19 +8,45 @@ TEST_FILES_PATH = Path(__file__).parent / "test_files" / "v1.0"
 
 
 @pytest.mark.parametrize(
-    ("xsd", "xml"), [(TEST_XSD, TEST_FILES_PATH / "valid.xml")], ids=["valid_xml"]
+    ("xsd", "xml"),
+    [
+        (TEST_XSD, TEST_FILES_PATH / "valid.xml"),
+        (
+            TEST_XSD,
+            '<?xml version="1.0" encoding="UTF-8"?><MetronInfo xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
+            "<Series><Name>Foo</Name></Series><Number /><PageCount>0</PageCount></MetronInfo>",
+        ),
+        (
+            TEST_XSD,
+            '<?xml version="1.0" encoding="UTF-8"?><MetronInfo xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
+            "<Series><Name>Foo</Name><Volume>0</Volume></Series><Number /></MetronInfo>",
+        ),
+    ],
+    ids=["valid_xml", "zero_page_count", "volume_zero"],
 )
-def test_valid(xsd, xml) -> None:
+def test_valid(xsd: Path, xml: Path | str) -> None:
     schema = XMLSchema11(xsd)
     schema.validate(xml)
 
 
 @pytest.mark.parametrize(
     ("xsd", "xml"),
-    [(TEST_XSD, TEST_FILES_PATH / "dup_primary_attr.xml")],
-    ids=["dup_primary_attr_xml"],
+    [
+        (TEST_XSD, TEST_FILES_PATH / "dup_primary_attr.xml"),
+        (
+            TEST_XSD,
+            '<?xml version="1.0" encoding="UTF-8"?><MetronInfo xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
+            "<Series><Name>Foo</Name></Series><Number /><PageCount>-1</PageCount></MetronInfo>",
+        ),
+        (
+            TEST_XSD,
+            '<?xml version="1.0" encoding="UTF-8"?><MetronInfo xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
+            "<Series><Name>Foo</Name><Volume>-1</Volume></Series><Number /></MetronInfo>",
+        ),
+    ],
+    ids=["dup_primary_attr_xml", "negative_page_count", "negative_volume"],
 )
-def test_invalid(xsd, xml) -> None:
+def test_invalid(xsd: Path, xml: Path | str) -> None:
     schema = XMLSchema11(xsd)
     with pytest.raises(XMLSchemaValidationError):
         schema.validate(xml)
